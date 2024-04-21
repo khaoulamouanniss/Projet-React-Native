c7bd837e1ea460d4010669446d7382a02b732657
khaoula <khaoula.mouanniss@gmail.com>
2024-04-21 16:17:17 -0400

    use picker-select instead of picker/picker

--- components/Selector.js
+++ components/Selector.js
@@ -3,10 +3,11 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Picker } from '@react-native-picker/picker'; //Install using: npm install @react-native-picker/picker
+import RNPickerSelect from 'react-native-picker-select'; //Install npm install react-native-picker-select
 
 const Selector = ({ selectedValue, onValueChange, items, item0, style, pickerStyle }) => (
     <View style={[styles.container, style]}>
-        <Picker
+       {/* <Picker
             selectedValue={selectedValue}
             onValueChange={(item) => onValueChange(item)}
             style={[styles.picker, pickerStyle]}
@@ -15,7 +16,14 @@ const Selector = ({ selectedValue, onValueChange, items, item0, style, pickerSty
             {items.map((item) => (
                 <Picker.Item key={item.value} label={item.label} value={item.value} />
             ))}
-        </Picker>
+            </Picker>*/}
+        <RNPickerSelect
+            onValueChange={onValueChange}
+            items={items.map(item => ({ key:item.value,label: item.label, value: item.value }))}
+            value={selectedValue}
+            placeholder={{ label: item0, value: null }}
+            style={[styles.picker, pickerStyle]}
+        />
     </View>
 );
 
